f0d85e5643d4faa0e1323b6bed391b350cd2c80e
veeraanji <veerakastech@gmail.com>
2019-09-07 17:40:08 +0530

    project-1

---
+++ project-1/src/main/java/kastech/App.java
@@ -0,0 +1,13 @@
+package kastech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
